72f54c74c5cda3cada0a61643d8d3d0b15b4e79c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-02-16 13:58:05 +0100

    Add Unicode test (#3)

--- test.js
+++ test.js
@@ -9,3 +9,10 @@ test('log levels', async (t) => {
   await t.execution(() => log.warn('This is a warning log'))
   await t.execution(() => log.error('This is an error log'))
 })
+
+test('unicode', async (t) => {
+  const log = new Log()
+
+  await t.execution(() => log.debug('Hëllø wørld'))
+  await t.execution(() => log.debug('😀'))
+})
